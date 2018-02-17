cae2a555f3aa81b895f8bc90e20acedc72a13064
bchugg <ben.ih.chugg@gmail.com>
2018-02-17 14:00:16 -0800

    add user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,31 @@
 class UsersController < ApplicationController
   def index
-  	@users = User.all
+  	@users = User.all.order(:created_at)
+  end
+
+  def show 
+  	@user = User.find(params[:id])
+  end
+
+  def new 
+  	@user = User.new
   end
 
   def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		redirect_to users_index_path
+  		flash[:success] = "Account created!"
+  	else 
+  		render 'new'
+  	end
   end
 
-  def update
-  end
+  private 
 
-  def destroy
-  end
+  	def user_params
+  		params.require(:user).permit(:username, :name, :email, :password, 
+  									:password_confirmation)
+  	end
+
 end
