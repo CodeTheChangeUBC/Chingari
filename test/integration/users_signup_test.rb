d6695d48cc286b936e426ddaa8c8faf7edb551fd
bchugg <ben.ih.chugg@gmail.com>
2018-02-17 14:25:53 -0800

    add user signup tests

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  
+  test "should not sign up invalid user" do 
+  	get new_user_path
+  	assert_no_difference 'User.count' do
+  		post users_path, params: { 
+  			user: { 
+  				name: "",
+  				email: "invalid",
+  				password: "pass",
+  				password_confirmation: "word"
+  			}
+  		}
+  	end
+  	assert_template 'users/new'
+  end
+
+  test "should signup a valid user" do 
+  	get new_user_path
+  	assert_difference 'User.count', 1 do 
+  		post users_path, params: {
+  			user: {
+  				name: "bella bib",
+  				email: "bella@gmail.ca",
+  				password: "foobaz",
+  				password: "foobaz"
+  			}
+  		}
+  	end
+  	follow_redirect!
+  	assert_template 'users/show'
+  end
+end
