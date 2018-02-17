c8ab5df0bc35c2dfe132e6490da41d77e3353bdc
bchugg <ben.ih.chugg@gmail.com>
2018-02-17 12:53:50 -0800

    add secure password functionality for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
   	validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-	# has_secure_password
+    validates :password, presence: true, length: { minimum: 6, maximum: 255}
+	has_secure_password
 end
