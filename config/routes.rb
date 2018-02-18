7c4110b9f218e6a5a42aa358268d9aac79a3e5d7
bchugg <ben.ih.chugg@gmail.com>
2018-02-17 16:04:31 -0800

    add static pages controller and header and footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
 
+	# Set root of application
+  root 'static_pages#home'
+
 	# Routes handled by Sessions
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
-  # Routes regarding users
+  # Routes pertaining to users
   get '/signup', to: 'users#new' # Nicer route name than /users/new
 	resources :users
 end
