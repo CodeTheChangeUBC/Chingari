4cec11ee62124c95a4e8060f637dec5b5c1b55ba
bchugg <ben.ih.chugg@gmail.com>
2018-02-17 16:50:24 -0800

    implement user logout and test both login and logout

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,16 +2,23 @@ module SessionsHelper
 
 	# Logs in user
 	def log_in(user)
-		sessions[:user_id] = user.id
+		session[:user_id] = user.id
 	end
 
 	# Returns current logged in user
 	def current_user
-		@current_user ||= User.find_by(id: sessions[:user_id])
+		@current_user ||= User.find_by(id: session[:user_id])
 	end
 
 	# Returns true iff current user is logged in
 	def logged_in? 
 		!current_user.nil?
 	end
+
+	# Logs out current user
+	def log_out
+		session.delete(:user_id)
+		@current_user = nil
+	end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,11 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
     validates :password, presence: true, length: { minimum: 6, maximum: 255}
 	has_secure_password
+
+	# Returns the hash digest of the given string.
+	def User.digest(string)
+		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+	                                              BCrypt::Engine.cost
+		BCrypt::Password.create(string, cost: cost)
+	end
 end
