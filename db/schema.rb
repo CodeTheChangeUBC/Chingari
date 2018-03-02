cd5939f37c92d2eea9336524f68761337b61f09d
Jimmy Lin <Jimmy@dhcp-128-189-255-169.ubcsecure.wireless.ubc.ca>
2018-03-01 18:56:46 -0800

    integrated main stylings

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,17 @@
 
 ActiveRecord::Schema.define(version: 20180216024515) do
 
+  create_table "courses", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "enrollments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
