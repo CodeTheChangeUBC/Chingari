c4c53e028a42a5d82197558394684e84d8497692
Eric Mikulin <ericm99@gmail.com>
2018-03-05 17:14:44 -0800

    Finished layout setup for the remaining pages & correctly linked images

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
     # Static pages roots
   get  '/home',     to: 'static_pages#index'
+  get  '/index',    to: 'static_pages#index'
   get  '/about',    to: 'static_pages#about'
   get  '/resources',to: 'static_pages#resources'
 
