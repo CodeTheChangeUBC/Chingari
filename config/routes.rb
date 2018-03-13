53c72521708af1be98fe41a37cbf43536e3d8366
Jimmy Lin <Jimmy@dhcp-128-189-250-162.ubcsecure.wireless.ubc.ca>
2018-03-12 18:17:28 -0700

    Fixed duplicate route declaration from previous merges

--- config/routes.rb
+++ config/routes.rb
@@ -12,16 +12,7 @@ Rails.application.routes.draw do
     get  '/index',    to: 'static_pages#index'
     get  '/about',    to: 'static_pages#about'
     get  '/resources',to: 'static_pages#resources'
-
-    # Set root of application
-    root 'static_pages#coming_soon'
-    
-      # Static pages roots
-    get  '/home',     to: 'static_pages#index'
-    get  '/index',    to: 'static_pages#index'
-    get  '/about',    to: 'static_pages#about'
-    get  '/resources',to: 'static_pages#resources'
-
+  
     # Routes handled by Sessions
     get    '/login',   to: 'sessions#new'
     post   '/login',   to: 'sessions#create'
