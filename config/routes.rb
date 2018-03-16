dfbf4a0d1dbb58fba054c54c4bb5f98848fc0745
MarshmallowCafe <ezutp@yahoo.com>
2018-03-15 20:05:08 -0700

    added partials for courses

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,31 @@
 Rails.application.routes.draw do
 	# Set root of application
   root 'static_pages#coming_soon'
-  
-  # Hidden routes for in-progress pages
-  if Rails.env == 'development' || Rails.env == 'test'
+
     # Static pages roots
-    get  '/home',     to: 'static_pages#index'
-    get  '/index',    to: 'static_pages#index'
-    get  '/about',    to: 'static_pages#about'
-    get  '/resources',to: 'static_pages#resources'
-  
-    # Routes handled by Sessions
-    get    '/login',   to: 'sessions#new'
-    post   '/login',   to: 'sessions#create'
-    delete '/logout',  to: 'sessions#destroy'
+  get  '/home',     to: 'static_pages#index'
+  get  '/index',    to: 'static_pages#index'
+  get  '/about',    to: 'static_pages#about'
+  get  '/resources',to: 'static_pages#resources'
 
-    # Routes pertaining to users
-    get '/signup', to: 'users#new' # Nicer route name than /users/new
-    resources :users
-  end
+	# Set root of application
+  root 'static_pages#coming_soon'
 
-  # Catch-all route
-  get '*path', to: 'static_pages#coming_soon'
+    # Static pages roots
+  get  '/home',     to: 'static_pages#index'
+  get  '/index',    to: 'static_pages#index'
+  get  '/about',    to: 'static_pages#about'
+  get  '/resources',to: 'static_pages#resources'
+
+	# Routes handled by Sessions
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  # Courses Routes
+  get '/test_courses', to: 'courses#view'
+
+  # Routes pertaining to users
+  get '/signup', to: 'users#new' # Nicer route name than /users/new
+	resources :users
 end
