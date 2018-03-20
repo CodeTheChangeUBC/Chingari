ac5ace267a4cc992a158b5455af6816d19b4d981
EC2 Default User <ec2-user@ip-172-31-19-175.ec2.internal>
2018-03-20 01:44:06 +0000

    ahhh

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,28 @@
 Rails.application.routes.draw do
 	# Set root of application
   root 'static_pages#coming_soon'
-
+  
+  # Hidden routes for in-progress pages
+  if Rails.env == 'development' || Rails.env == 'test'
     # Static pages roots
-  get  '/home',     to: 'static_pages#index'
-  get  '/index',    to: 'static_pages#index'
-  get  '/about',    to: 'static_pages#about'
-  get  '/resources',to: 'static_pages#resources'
+    get  '/home',     to: 'static_pages#index'
+    get  '/index',    to: 'static_pages#index'
+    get  '/about',    to: 'static_pages#about'
+    get  '/resources',to: 'static_pages#resources'
+  
+    # Routes handled by Sessions
+    get    '/login',   to: 'sessions#new'
+    post   '/login',   to: 'sessions#create'
+    delete '/logout',  to: 'sessions#destroy'
 
-	# Set root of application
-  root 'static_pages#coming_soon'
-
-    # Static pages roots
-  get  '/home',     to: 'static_pages#index'
-  get  '/index',    to: 'static_pages#index'
-  get  '/about',    to: 'static_pages#about'
-  get  '/resources',to: 'static_pages#resources'
-
-	# Routes handled by Sessions
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
-
-<<<<<<< HEAD
-  # Courses Routes
-  get '/test_courses', to: 'courses#view'
-=======
     # Routes pertaining to users
     get '/signup', to: 'users#new' # Nicer route name than /users/new
     get '/preview_profile', to: 'users#preview_profile'
     resources :users
+    
+    get '/test_courses', to: 'courses#view'
   end
->>>>>>> 3d6a30f5c2a36cb4e91fa516dae431dd93336f64
 
-  # Routes pertaining to users
-  get '/signup', to: 'users#new' # Nicer route name than /users/new
-	resources :users
+  # Catch-all route
+  get '*path', to: 'static_pages#coming_soon'
 end
