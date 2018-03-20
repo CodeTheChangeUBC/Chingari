818f9c9c7ea9b4a1e5aeb6cdb2e23c426dacd524
EC2 Default User <ec2-user@ip-172-31-19-175.ec2.internal>
2018-03-20 01:32:10 +0000

    changes

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,5 +13,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
-Rails.application.config.assets.precompile += %w( static.scss )
+Rails.application.config.assets.precompile += %w( static.scss courses_small.scss )
 Rails.application.config.assets.precompile += %w( static.js )
