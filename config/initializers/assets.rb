bc6ff4b2258a480437b177bc7fbf05538627551d
Jimmy Lin <jimmy.lin1313@gmail.com>
2018-04-13 04:33:14 -0700

    Finished cleaning up the home page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,5 +13,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
-Rails.application.config.assets.precompile += %w( static.scss )
-Rails.application.config.assets.precompile += %w( static.js )
+Rails.application.config.assets.precompile += %w(  )
+Rails.application.config.assets.precompile += %w( home.scss )
