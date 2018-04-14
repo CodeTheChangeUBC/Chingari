2b325ed638486009510008ac4e7ec59dd2b0ae9b
Jimmy Lin <jimmy.lin1313@gmail.com>
2018-04-13 20:12:01 -0700

    Fixed small issue with hash updates on single-page navigation
    I also moved the debounce state into the AnchorScroller class as a static variable. This way we are less likely to accidentally conflict with other components for namespace

--- app/assets/javascripts/anchor_scroller.js
+++ app/assets/javascripts/anchor_scroller.js
@@ -1,28 +1,31 @@
-// "use strict";
-// Object.defineProperty(exports, "__esModule", { value: true });
 class AnchorScroller {
-
-
-
-  // Calling this scrolls to the current location hash (if possible)
+  // Summary: 
+  //   Calling this scrolls to the current location hash id (eg. /current/path/#id) if one exists.
+  // DOM Effects: 
+  //   For this to work
+  //     - There must be an element with a matching data-anchor-id
+  //     - The scroller must have finished all current actions
+  //   Otherwise the action is ignored.
+  //   Note that the scroll destination is computed after the delay, just before animation.
+  // Params:
+  //   delay - scrolling animation delay in milliseconds
+  //   duration - scrolling animation duration in milliseconds
   static scrollToCurrentAnchor(delay, duration) {
     if (document.location.hash.length >= 1) {
       AnchorScroller.scrollToDataAnchorDelay(document.location.hash.substr(1), delay, duration);
     }
   }
 
-  // Scroll to the first anchor element with a matching data-anchor-id
-  // Can specify delay and duration
   static scrollToDataAnchorDelay(id, delay, duration) {
     setTimeout(AnchorScroller.scrollToDataAnchor, delay, id, duration);
   }
 
   static scrollToDataAnchor(id, duration) {
-    if (document.scrollDebounce === false) {
-      document.scrollDebounce = true
+    if (AnchorScroller.scrollDebounce === false) {
+      AnchorScroller.scrollDebounce = true
       let element = AnchorScroller.getElementByDataAnchorId(id)
       if (element === undefined) {
-        document.scrollDebounce = false
+        AnchorScroller.scrollDebounce = false
         return
       } else {
         $("html, body").animate(
@@ -30,7 +33,7 @@ class AnchorScroller {
           duration,
           () => { 
             setTimeout(() => {
-              document.scrollDebounce = false
+              AnchorScroller.scrollDebounce = false
             }, 100) 
           }
         );
@@ -43,7 +46,6 @@ class AnchorScroller {
     return matches.length >= 1 ? matches[0] : undefined
   }
 
-
   static overrideAnchorClick(event, delay, duration) {
     if (event.delegateTarget.getAttribute("href") == null) { return }
     let hash_match = event.delegateTarget.getAttribute("href").match(/#\w+/)
@@ -51,6 +53,7 @@ class AnchorScroller {
       let hash = hash_match[0].substr(1)
       if (AnchorScroller.getElementByDataAnchorId(hash) !== undefined) {
         event.preventDefault();
+        document.location.hash = hash;
         AnchorScroller.scrollToCurrentAnchor(1, 500)
       }
     }
@@ -58,41 +61,4 @@ class AnchorScroller {
 
 }
 
-document.scrollDebounce = false;
-
-// // Scroll to the first anchor element with a matching data-anchor-id
-// // Can specify delay and duration
-// function scrollToDataAnchorDelay(id, delay, duration) {
-//   if (window.scrollDebounce === false) {
-//     window.scrollDebounce = true
-//     setTimeout(scrollToElement, delay, id, duration);
-//   }
-// }
-
-// // Calling this scrolls to the current location hash (if possible)
-// function scrollToAnchor(delay, duration) {
-//   if (document.location.hash.length >= 1) {
-//     scrollToDataAnchorDelay(document.location.hash.substr(1), delay, duration);
-//   }
-// }
-
-// /* PRIVATE METHODS */
-
-// function getElementByDataAnchorId(id) {
-//   let matches = $(document).find("a[data-anchor-id='" + id + "']");
-//   return matches.length >= 1 ? matches[0] : undefined
-// }
-
-// function scrollToDataAnchor(id, duration) {
-//   let element = getElementByDataAnchorId(id)
-//   if (element === undefined) {
-//     window.scrollDebounce = false
-//     return
-//   } else {
-//     $("html, body").animate({
-//       scrollTop: $(element).offset().top,
-//       duration: duration,
-//       complete: () => { setTimeout(() => window.scrollDebounce = false, 100) }
-//     });
-//   }
-// }
+AnchorScroller.scrollDebounce = false;
