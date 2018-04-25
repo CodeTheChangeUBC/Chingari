c0affa4df345c1e535e91385e4ef1d2d72669fcd
Jimmy Lin <jimmy.lin1313@gmail.com>
2018-04-24 20:25:55 -0700

    Finished initial Course UI - Finished Refactoring structure to separate structure from content - Finished connecting and testing transactional end-point modelling - Need to Finish styling some custom rendering - Need to isolate UI code into proper structure

--- app/assets/javascripts/models/course.js
+++ app/assets/javascripts/models/course.js
@@ -2,26 +2,13 @@ class CourseModel {
     // If any of these get an error code, the promise should reject with <status: error_code, result: explanation>
 
     static parse_response(response) {
-        response.status = response.status
-        return response
+        const new_response = response.responseJSON
+        new_response.status = response.status
+        return new_response
     }
 
-    // .index() => Promise<status: success_code, result: existing_courses>
-    static index() {
-        return new Promise(function(resolve, reject){
-            let url = "/courses";
-            $.ajax({
-                url: url,
-                type: 'GET',
-                success(data, status, xobj) { resolve(CourseModel.parse_response(xobj)) },
-                error(data, status, xobj) { reject(CourseModel.parse_response(data)) }
-            });
-        })
-    }
-
-    static drafts() {
+    static send_get(url) {
         return new Promise(function (resolve, reject) {
-            let url = "/courses/drafts";
             $.ajax({
                 url: url,
                 type: 'GET',
@@ -29,105 +16,34 @@ class CourseModel {
                 error(data, status, xobj) { reject(CourseModel.parse_response(data)) }
             });
         })
-    }    
+    }
 
-    static review() {
+    static send_post(url, data) {
         return new Promise(function (resolve, reject) {
-            let url = "/courses/review";
-            $.ajax({
-                url: url,
-                type: 'GET',
-                success(data, status, xobj) { resolve(CourseModel.parse_response(xobj)) },
-                error(data, status, xobj) { reject(CourseModel.parse_response(data)) }
-            });
-        })
-    }    
-
-    static published() {
-        return new Promise(function (resolve, reject) {
-            let url = "/courses/published";
-            $.ajax({
-                url: url,
-                type: 'GET',
-                success(data, status, xobj) { resolve(CourseModel.parse_response(xobj)) },
-                error(data, status, xobj) { reject(CourseModel.parse_response(data)) }
-            });
-        })
-    }
-
-    // .show(id) => Promise<status: success_code, result: existing_course>
-    static show(id) {
-        return new Promise(function(resolve, reject){
-            let url = "/courses/"+id;
-            $.ajax({
-                url: url,
-                type: 'GET',
-                success(data, status, xobj) { resolve(CourseModel.parse_response(xobj)) },
-                error(data, status, xobj) { reject(CourseModel.parse_response(data)) }
-            });
-        })
-    }
-
-    // .new() => Promise<status: success_code, result: new_course, schema: course_schema>
-    static new() {
-        return new Promise(function(resolve, reject){
-            let url = "/courses/new";
-            $.ajax({
-                url: url,
-                type: 'GET',
-                success(data, status, xobj) { resolve(CourseModel.parse_response(xobj)) },
-                error(data, status, xobj) { reject(CourseModel.parse_response(data)) }
-            });
-        })
-    }
-
-    // .edit(id) => Promise<status: success_code, result: existing_course, schema: course_schema>
-    static edit(id) {
-        return new Promise(function(resolve, reject){
-            let url = "/courses/"+id+"/edit";
-            $.ajax({
-                url: url,
-                type: 'GET',
-                success(data, status, xobj) { resolve(CourseModel.parse_response(xobj)) },
-                error(data, status, xobj) { reject(CourseModel.parse_response(data)) }
-            });
-        })
-    }
-
-    // .create(<form_params>) => Promise<status: success_code, result: existing_course>]
-    // create and update passes the new/edited course as the <form_params> object. The renderer should know how to handle filling in the data based on the schema received from the server.
-    static create(params) {
-        return new Promise(function(resolve, reject){
-            let url = "/courses";
             $.ajax({
                 url: url,
                 type: 'POST',
-                data: params,
+                data: data,
                 success(data, status, xobj) { resolve(CourseModel.parse_response(xobj)) },
                 error(data, status, xobj) { reject(CourseModel.parse_response(data)) }
             });
         })
     }
 
-    // .update(id, <form_params>) => Promise<status: success_code, result: existing_course>
-    // create and update passes the new/edited course as the <form_params> object. The renderer should know how to handle filling in the data based on the schema received from the server.
-    static update(id, params) {
-        return new Promise(function(resolve, reject){
-            let url = "/courses/"+id;
+    static send_put(url, data) {
+        return new Promise(function (resolve, reject) {
             $.ajax({
                 url: url,
                 type: 'PUT',
-                data: params,
+                data: data,
                 success(data, status, xobj) { resolve(CourseModel.parse_response(xobj)) },
                 error(data, status, xobj) { reject(CourseModel.parse_response(data)) }
             });
         })
     }
 
-    // .delete(id) => Promise<status: success_code, result: message>
-    static delete(id) {
-        return new Promise(function(resolve, reject){
-            let url = "/courses/"+id;
+    static send_delete(url) {
+        return new Promise(function (resolve, reject) {
             $.ajax({
                 url: url,
                 type: 'DELETE',
@@ -135,5 +51,54 @@ class CourseModel {
                 error(data, status, xobj) { reject(CourseModel.parse_response(data)) }
             });
         })
+    }
+
+    // .index() => Promise<status: success_code, result: existing_courses>
+    static index() {
+        return CourseModel.send_get("/courses")
+    }
+
+    static drafts() {
+        return CourseModel.send_get("/courses/drafts")
+    }    
+
+    static review() {
+        return CourseModel.send_get("/courses/review")
+    }    
+
+    static published() {
+        return CourseModel.send_get("/courses/published")
+    }
+
+    // .show(id) => Promise<status: success_code, result: existing_course>
+    static show(id) {
+        return CourseModel.send_get("/courses/" + id)
+    }
+
+    // .new() => Promise<status: success_code, result: new_course, schema: course_schema>
+    static new() {
+        return CourseModel.send_get("/courses/new")
+    }
+
+    // .edit(id) => Promise<status: success_code, result: existing_course, schema: course_schema>
+    static edit(id) {
+        return CourseModel.send_get("/courses/" + id + "/edit")
+    }
+
+    // .create(<form_params>) => Promise<status: success_code, result: existing_course>]
+    // create and update passes the new/edited course as the <form_params> object. The renderer should know how to handle filling in the data based on the schema received from the server.
+    static create(params) {
+        return CourseModel.send_post("/courses", { course: params })
+    }
+
+    // .update(id, <form_params>) => Promise<status: success_code, result: existing_course>
+    // create and update passes the new/edited course as the <form_params> object. The renderer should know how to handle filling in the data based on the schema received from the server.
+    static update(id, params) {
+        return CourseModel.send_put("/courses/" + id, { course: params })
+    }
+
+    // .delete(id) => Promise<status: success_code, result: message>
+    static delete(id) {
+        return CourseModel.send_delete("/courses/" + id)
     }
 }
---
+++ app/models/authorization.rb
@@ -0,0 +1,77 @@
+class Authorization
+  # List of User Roles translated into their database encoding
+
+  def self.permissions_setting
+    {
+      course: {
+        owner: {
+          create: {
+
+          },
+          read: {
+
+          },
+          update: {
+
+          },
+          delete: true
+        },
+        admin: {
+          create: true,
+          read: true
+          edit: true,
+          delete: true
+        },
+        moderator: {
+
+        },
+        user: {
+          
+        }
+      },
+      event: {
+
+      },
+      resource: {
+
+      }
+    }
+  end
+
+  def self.permissions(item, user)
+
+  end
+
+  # Description: Cannot make any state changes to database (create, update, delete)
+  def self.deactivated
+    Role.schema[:deactivated]
+  end
+
+  # Responsiblities: Manage roles
+  # Policies:
+  # Can create, submit, and publish content
+  # Can view all self-created content
+  # Can view all submitted and published content
+  # Can change roles of any user
+  def self.admin
+    Role.schema[:admin]
+  end
+
+  # Responsibilities: Publish Content
+  # Policies:
+  # Can create, submit, and publish content
+  # Can view all self-created content
+  # Can view all submitted and published content
+  def self.moderator
+    Role.schema[:moderator]
+  end
+
+  # Responsiblities: Create Content
+  # Policies:
+  # Can create and submit content
+  # Can view all self-created content 
+  # Can view all published content of qualified tier
+  def self.user
+    Role.schema[:user]
+  end
+end
