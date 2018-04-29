aa3e761f65829b650aabe0b3404ff469a95c79de
Eric Mikulin <ericm99@gmail.com>
2018-04-29 16:15:00 -0700

    Added routes to stage

--- config/routes.rb
+++ config/routes.rb
@@ -74,6 +74,15 @@ Rails.application.routes.draw do
     delete '/courses/:course_id', to: 'courses#delete' # ^^^^
     get '/courses/:course_id/edit', to: 'courses#edit'  # ^^^^
 
+    # Attachments API
+    get '/courses/(:course_id)/attachments', to: 'courses#attachment_index'
+    post '/courses/(:course_id)/attachments', to: 'courses#attachment_create'
+    get '/courses/(:course_id)/attachments/documents/(:attach_id)', to: 'courses#attachment_get', defaults: { type: 'Document' }
+    get '/courses/(:course_id)/attachments/embeds/(:attach_id)', to: 'courses#attachment_get', defaults: { type: 'Embed' }
+    put '/courses/(:course_id)/attachments/(:attach_id)', to: 'courses#attachment_edit'
+    delete "/courses/(:course_id)/attachments/documents/(:attach_id)", to: 'courses#attachment_delete', defaults: { type: 'Document' }
+    delete "/courses/(:course_id)/attachments/embeds/(:attach_id)", to: 'courses#attachment_delete', defaults: { type: 'Embed' }
+
   # Released routes
   elsif Rails.env == 'production'
     # Set root of application
